455ff10bb5739d4d9333491981685121ae86ed03
LanceRemigio <lancepants@DESKTOP-AFINSSH>
2023-09-16 01:26:17 -0700

    finished redoing paper

---
+++ simulation/test.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.integrate as int
+
+# Weights
+# Repulsiveness between of robots 
+
+c_1 = 1 
+
+# Repulsivness of obstacles
+
+c_2 = 3
+
+
+# Robots
+#robot1_start = #insert initial position for robot 1
+#robot1_end = #insert final position for robot 1
+#robot2_start = #insert initial position for robot 2
+#robot2_end = #insert final position for robot 2
+##
+## Obstacles
+#obs1_radius = #specify size of radius of obstacle
+#obs1_pos = #specify location of center of obstacle
+##
+#robot1_x = #insert guess for x position for robot 1
+#robot1_y = #insert guess for y position for robot 1
+#robot2_x = #insert guess for x position for robot 2
+#robot2_y = #insert guess for y position for robot 2
+
+

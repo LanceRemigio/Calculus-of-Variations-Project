a9f6e8d6353cbff3e642f3a6b23060893387cbf2
LanceRemigio <lancepants@DESKTOP-AFINSSH>
2023-09-16 13:46:22 -0700

    added stuff to .gitignore

--- simulation/test.py
+++ simulation/test.py
@@ -13,18 +13,18 @@ c_2 = 3
 
 
 # Robots
-#robot1_start = #insert initial position for robot 1
-#robot1_end = #insert final position for robot 1
-#robot2_start = #insert initial position for robot 2
-#robot2_end = #insert final position for robot 2
+robot1_start = (1,2) #insert initial position for robot 1
+robot1_end = (3,1) #insert final position for robot 1
+robot2_start = (0,1)#insert initial position for robot 2
+robot2_end = (5,1)#insert final position for robot 2
 ##
 ## Obstacles
-#obs1_radius = #specify size of radius of obstacle
-#obs1_pos = #specify location of center of obstacle
+obs1_radius = 4 #specify size of radius of obstacle
+obs1_pos = (0,0) #specify location of center of obstacle
 ##
-#robot1_x = #insert guess for x position for robot 1
-#robot1_y = #insert guess for y position for robot 1
-#robot2_x = #insert guess for x position for robot 2
-#robot2_y = #insert guess for y position for robot 2
+robot1_x =  0  #insert guess for x position for robot 1
+robot1_y = 0  #insert guess for y position for robot 1
+robot2_x = 0 #insert guess for x position for robot 2
+robot2_y = 0 #insert guess for y position for robot 2
 
 

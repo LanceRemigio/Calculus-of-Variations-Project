7d9a60f2f9ee2b527f0ec989b1639b733f65fe5b
LanceRemigio <lancepants@DESKTOP-AFINSSH>
2023-09-26 23:29:51 -0700

    adjustments

--- simulation/test.py
+++ simulation/test.py
@@ -28,3 +28,36 @@ robot2_x = 0 #insert guess for x position for robot 2
 robot2_y = 0 #insert guess for y position for robot 2
 
 
+# -(c1 * (x1 - x2)) / ((x1 - x2) ** 2 + (y1 - y2) ** 2 )
+# - c2 * (x1 - 2) / (np.sqrt((x1 - obs1_pos[0])) ** 2 
+#  (y1 - obs1_pos[1]) **2) - obs1_radius) ** 3 / np.sqrt((x1 - obs_pos[0]) ** 2 + (y1 - obs1_pos[1]) ** 2)
+
+# 
+
+# This function is doing too much 
+# Original function is unreadable 
+
+
+def diff_func (x1,y1, x2,y2, rep_obj, rep_robot):
+    delt_x = x1 - x2
+    delt_y = y1 - y2
+    
+    x1_el = -(rep_obj * delt_x) / (delt_x ** 2 + delt_y ** 2 ) ** 2 - rep_robot * (x1 - 2 ) / (np.sqrt((x1 - obs1_pos[0]) ** 2 + ))
+    
+
+    return 0    
+
+def boundary_conditions (ya, yb):
+    return ya[0] - robot1_start[0], yb[0] - robot1_end[0], ya[1] - robot1_start[1], yb[1] - robot1_end[1], ya[2] - robot2_start[0], yb[2] - robot2_end[0], ya[3] - robot2_start[1], yb[3] - robot2_end[1]
+
+t_guess = np.linspace(0,1,5) 
+
+xy_guess = np.zeros((8, t_guess.size))
+
+xy_guess[0] = robot1_x
+xy_guess[1] = robot1_y
+xy_guess[2] = robot2_x
+xy_guess[3] = robot2_y
+
+res = int.solve_bvp(diff_func, boundary_conditions, t_guess, xy_guess)
+

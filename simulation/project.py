e5b77caaf5edbfe38f42095603d9c0c07d767cee
LanceRemigio <lancejairo.remigio99@gmail.com>
2024-01-16 00:01:05 -0800

    more updates

--- simulation/project.py
+++ simulation/project.py
@@ -1,18 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.integrate as int
+import scipy.integrate as integrate
 
 # weights
 
 C1 = 1 
 C2 = 1 
 
+# user input for testing
+
+# boundary conditions for first robot
+robot1i = input("Enter starting coordinate of first robot: ")
+robot1f = input("Enter ending coordinate of first robot:  ")
+
+robot1_start = tuple([int(item) for item in robot1i.split(',')])
+robot1_end = tuple([int(item) for item in robot1f.split(',')])
+
+# boundary conditions for second robot
+robot2i = input("Enter starting coordinate of second robot: ")
+robot2f = input("Enter ending coordinate of second robot:  ")
+
+robot2_start = tuple([int(item) for item in robot2i.split(',')])
+robot2_end = tuple([int(item) for item in robot2f.split(',')])
+
+
 # Boundary Conditions
 
-robot1_start = (1,0)
-robot1_end = (2,1)
-robot2_start = (0,0)
-robot2_end = (1,5)
+# robot1_start = (1,0)
+# robot1_end = (2,1)
+# robot2_start = (0,0)
+# robot2_end = (1,5)
 
 # Guess
 
@@ -44,7 +61,7 @@ xy_guess[1] = robot1_y
 xy_guess[2] = robot2_x
 xy_guess[3] = robot2_y
 
-res = int.solve_bvp(dxydt, boundCond, t_guess, xy_guess)
+res = integrate.solve_bvp(dxydt, boundCond, t_guess, xy_guess)
 
 # Paths of Robots
 
@@ -54,7 +71,7 @@ obs_t = np.linspace(0, 2 * np.pi, 100)
 plt.plot(res.sol(t)[0], res.sol(t)[1], label = 'Robot 1')
 plt.plot(res.sol(t)[2], res.sol(t)[3], label = 'Robot 2')
 
-plt.plot(np.cos(obs_t) + 2, np.sin(obs_t) + 2, label = 'obstacle')
+plt.plot(np.cos(obs_t) + 5, np.sin(obs_t) + 5, label = 'obstacle')
 plt.legend()
 
 plt.show()
---
+++ simulation/test.py
@@ -0,0 +1,9 @@
+usr_in = input('Input a tuple: ')
+
+x = [int(item) for item in usr_in.split(',')]
+
+print(tuple(x))
+
+
+
+
